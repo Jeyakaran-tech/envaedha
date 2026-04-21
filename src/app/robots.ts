5c77146855b3c9c47a57bff57d980733977d789b
Jeyakaran Karnan <jeyakaran30@gmail.com>
2026-04-21 23:24:21 +1000

    fix: update base URL to include www subdomain in robots.txt and sitemap configuration

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://envaedha.com.au/sitemap.xml',
+    sitemap: 'https://www.envaedha.com.au/sitemap.xml',
   }
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://envaedha.com.au'
+  const baseUrl = 'https://www.envaedha.com.au'
 
   return [
     {
